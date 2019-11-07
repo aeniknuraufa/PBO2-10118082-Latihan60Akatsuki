2caa8984e6b32a36560bcc021cfdd0594fb9ffb4
Aenik Nur Aufa <aeniknuraufa@users.noreply.github.com>
2019-11-07 23:08:32 +0700

    PBO2-10118082-Latihan60
    Membuat program dengan pendekatan berbasis objek untuk menampilkan keanggotaan akatsuki berdasarkan desa atau asal

---
+++ src/pbo2/pkg10118082/latihan60/akatsuki/Tester.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118082.latihan60.akatsuki;
+
+import java.util.Scanner;
+
+/**
+ *	
+ * @author
+ * Nama     : Aenik Nur Aufa
+ * Kelas    : IF 2
+ * NIM      : 10118082
+ * Deskripsi Program : Membuat program dengan pendekatan berbasis objek untuk menampilkan
+ * keanggotaan akatsuki berdasarkan desa atau asal
+ */
+public class Tester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Akatsuki akatsuki = new Akatsuki();
+        
+        System.out.println("===Tokoh Akatsuki Berdasarkan Desa/Asal===");
+        akatsuki.tampilDaftarDesa();
+        akatsuki.setDesa(sc.nextInt());
+        
+        System.out.println("");
+        
+        akatsuki.tampilAnggotaDesa(akatsuki.getDesa());
+    }
+    
+}
